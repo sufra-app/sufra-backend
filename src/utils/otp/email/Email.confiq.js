fa862f050fc2b8ace732ccb711b368eb0ff771d7
Sajaalghoul <41466655+Sajaalghoul@users.noreply.github.com>
2025-12-28 21:49:06 +0200

    Export transporter for email configuration

--- src/utils/otp/email/Email.confiq.js
+++ src/utils/otp/email/Email.confiq.js
@@ -2,7 +2,7 @@ import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 dotenv.config();
 
-const transporter = nodemailer.createTransport({
+export const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
